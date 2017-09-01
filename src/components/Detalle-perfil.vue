27c8ba96d1d2a56237ef72278c32d23a99935d1f
kevin urbano <kevurbmac@gmail.com>
2017-09-01 18:41:00 +0200

    Kevin subiendo Maestro-perfil y Detalles-perfil

---
+++ src/components/Detalle-perfil.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<div>
+		  <h3 style="overflow: hidden; text-overflow: ellipsis; max-width:400px"><strong>Perfil: </strong>{{Perfil.Nombre}}</h3>
+		  <label class="w3-text" for="nombre"> Nombre </label>
+		  <input class="w3-input w3-border" style="overflow: hidden; text-overflow: ellipsis" type="string" name="nombre" value="Nombre" :disabled="!editing && !addingNew" v-model="Perfil.Nombre">
+		  <label class="w3-text" for="descripcion"> Descripción </label>
+		  <input class="w3-input w3-border" style="overflow: hidden; text-overflow: ellipsis" type="string" name="descripcion" value="Descripción" :disabled="!editing && !addingNew" v-model="Perfil.Descripción">
+		  <label class="w3-text" for="departamento"> Departamento </label>
+		  <input class="w3-input w3-border" style="overflow: hidden; text-overflow: ellipsis" type="string" name="departamento" value="Departamento" :disabled="!editing && !addingNew" v-model="Perfil.Descripcion">
+		  <label class="w3-text" for="edad-media"> Edad Media </label>
+		  <input class="w3-input w3-border" style="overflow: hidden; text-overflow: ellipsis" type="numeric" name="edad-media" value="Edad-Media" :disabled="!editing && !addingNew" v-model="Perfil.Edad-Media"><
+
+		  <!--  falta agregar campo de Administrador  -->
+		</div>
+
+
+
+
+
+
+
+
+
+	</div>
+</template>>
---
+++ src/components/Maestro-perfil.vue
@@ -0,0 +1,61 @@
+<template>
+  <div style="min-width:400px; max-width:400px; display:inline-block; vertical-align:top">
+    <ul class="w3-ul w3-card-4">
+      <li><h2><strong>Perfiles de Usuarios</strong></h2></li>
+      <li style="overflow: hidden; text-overflow: ellipsis" class="w3-hover-blue" v-for="Perfil in Perfiles" @click="perfilSelected(Perfil.Id)"> Título: {{Perfil.nombre}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+
+import EventBus from './event-bus.js'
+import AppIcon from './App-icon.vue'
+import appConfig from './config.js'
+
+var httpURL = appConfig.URLPerfil;
+
+export default {
+  components: {
+    'app-icon' : AppIcon
+  },
+  methods: {
+  	loadList: function(){
+      var _this = this;
+      $.ajax(
+        {
+          url : httpURL,
+          type: "GET",
+        })
+        .done(function(data) {
+          _this.Perfiles = data;
+        })
+        .fail(function(data) {
+          let mensaje = 'No se pudo cargar la lista. Revise su conexión a Internet.';
+          EventBus.$emit('showMessage', mensaje);
+        });
+    },
+
+    peliculaSelected: function(id){
+    	EventBus.$emit('perfilSelected', id);
+    }
+
+  },
+
+  data: function () {
+	  return {
+	    Perfiles: [],
+	  }
+  },
+
+  mounted: function() {
+      this.loadList();
+
+	  EventBus.$on('updateListPerfil', function() {
+	    this.loadList();
+	  }.bind(this));
+  }
+
+}
+
+</script>
